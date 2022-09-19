a5a390df568660fac9b9b1e0a2deaa3f75d0dcc4
Tyson Lupul <wavesrcool@icloud.com>
2022-09-19 12:23:11 -0500

    build(root): update working tree

--- packages/async/src/index.ts
+++ packages/async/src/index.ts
@@ -2,7 +2,7 @@
  * * Radroots Documentation
  *
  * @collection @radroots/async
- * @created 8 18 2022
+ * @created 8 19 2022
  *
  */
 export { RadrootsAsyncPromiseCatch as PromiseCatch } from "./async/RadrootsAsyncPromiseCatch";
--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -2,7 +2,7 @@
  * * Radroots Documentation
  *
  * @collection @radroots/library
- * @created 8 18 2022
+ * @created 8 19 2022
  *
  */
 export { RadrootsLibraryTypesRadroots as TypesRadroots } from "./types/radroots/RadrootsLibraryTypesRadroots";
